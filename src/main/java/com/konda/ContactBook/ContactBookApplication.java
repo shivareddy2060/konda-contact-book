6765ff9ab18c08f2a2b801b41515f9a48cc7a657
shivareddy2060 <shivareddy20051995@gmail.com>
2021-12-11 00:48:38 +0530

    contact book

---
+++ src/main/java/com/konda/ContactBook/ContactBookApplication.java
@@ -0,0 +1,13 @@
+package com.konda.ContactBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactBookApplication.class, args);
+	}
+
+}
